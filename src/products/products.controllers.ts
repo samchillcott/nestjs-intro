414592a8de5900e8560e4c1b2e90c3fb72db6b89
Sam Chillcott <sam@secret-source.eu>
2021-06-16 09:26:51 +0100

    final products files added

--- src/products/products.controllers.ts
+++ src/products/products.controllers.ts
@@ -1,9 +1,21 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
     @Post()
-    addProduct(): any {
-        
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number,
+    ) {
+        const generatedId = this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice,
+        );
+        return { id: generatedId };
     }
 }
